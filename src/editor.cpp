775b0210111f24a903ea4a569da2c94630f14f67
cmsteffey <65865777+cmsteffey@users.noreply.github.com>
2026-03-16 17:00:57 -0400

    Individual files support

--- src/editor.cpp
+++ src/editor.cpp
@@ -10,17 +10,23 @@
 #include <iostream>
 #include <stdlib.h>
 #include "TextEditor.h"
+#include <vector>
 #define SDL_HINT_FILE_DIALOG_DRIVER "zenity"
 // Constant definition for default window width.
-#define WIN_WIDTH (160 * 4)
+#define WIN_WIDTH (250 * 4)
 // Constant definition for default window height.
-#define WIN_HEIGHT (144 * 4)
+#define WIN_HEIGHT (150 * 4)
 static void SDLCALL fileopen_callback(void* userdata, const char* const* filelist, int filter);
 static void SDLCALL filesaveas_callback(void* userdata, const char* const* filelist, int filter);
 static void write_to_path(const char* const path, std::string text);
+static std::string load_from_path(const char* const path);
+
 // Semaphore held during callbacks and render loop, to synchonize execution of callback functions.
 sem_t renderSemaphore;
-char* openpath;
+std::vector<char*> openPaths;
+std::vector<bool> modifiedPaths;
+std::vector<std::string> contents;
+int openIndex = -1;
 int main(void) {
   //Initialize the semaphore before any code, non-process shared with a initial value of 1
   if(sem_init(&renderSemaphore, 0, 1)){
@@ -66,11 +72,19 @@ int main(void) {
   ImGui_ImplSDLRenderer3_Init(renderer);
 
   TextEditor editor;
-  auto lang = TextEditor::LanguageDefinition::CPlusPlus();
-  editor.SetLanguageDefinition(lang);
+  TextEditor::LanguageDefinition def;
+  def.mAutoIndentation = true;
+  def.mSingleLineComment = "#";
+
+  def.mKeywords.insert("data");
+  def.mKeywords.insert("unit");
+  def.mKeywords.insert("u16");
+
+  editor.SetLanguageDefinition(def);
   editor.SetPalette(TextEditor::GetLightPalette());
-  editor.SetText("\x2f\x2f Open a file in the menu to begin");
+  editor.SetText("#Open a file in the menu to begin");
   editor.SetHandleKeyboardInputs(true);
+  editor.SetColorizerEnable(true);
   SDL_StartTextInput(window);
   // Sentinel value for event loop.
   bool done = false;
@@ -79,24 +93,15 @@ int main(void) {
     sem_wait(&renderSemaphore);
     // Handle for tracking SDL events within event loop.
     SDL_Event event;
-
+    bool keyUpInFrame = false;
     // Handle event loops in a continuous stream per update.
     while (SDL_PollEvent(&event)) {
       // ImGui needs to be synchronised against SDL window events.
       ImGui_ImplSDL3_ProcessEvent(&event);
-
+      if (event.type == SDL_EVENT_KEY_UP)
+        keyUpInFrame = true;
       //Make Shift-Enter enter a new line, because I do this when typing brackets and IGTE doesn't do it by default.
       if(event.type == SDL_EVENT_KEY_DOWN && event.key.key == SDLK_RETURN && event.key.mod & (SDL_KMOD_LSHIFT|SDL_KMOD_RSHIFT)){
-        /*SDL_Event manufacturedDown;
-        SDL_Event manufacturedUp;
-        SDL_memset(&manufacturedDown, 0, sizeof(manufacturedDown));
-        SDL_memset(&manufacturedUp, 0, sizeof(manufacturedUp));
-        manufacturedDown.type = SDL_EVENT_KEY_DOWN;
-        manufacturedUp.type = SDL_EVENT_KEY_UP;
-        manufacturedUp.key.key =
-          manufacturedDown.key.key = SDLK_RETURN;
-        SDL_PushEvent(&manufacturedDown);
-        SDL_PushEvent(&manufacturedUp);*/
         editor.InsertText("\n");
       }
       // Check for any exit requests and set sentinel accordingly.
@@ -115,19 +120,20 @@ int main(void) {
     ImGui::SetNextWindowPos(ImVec2(0.0f, 0.0f));
     ImGui::SetNextWindowSize(io.DisplaySize);
     ImGui::Begin("Hello, World!", NULL, ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoMove);
-    ImGui::SetWindowSize(ImVec2(300, 300), ImGuiCond_FirstUseEver);
+    ImGui::SetWindowSize(ImVec2(400, 600), ImGuiCond_FirstUseEver);
 
-    // Have a file menu for save and open
+    // Have a file menu for save, open, and run
     if(ImGui::BeginMenuBar()){
       if(ImGui::BeginMenu("File")){
         if(ImGui::MenuItem("Open...", "Ctrl-O")){
           SDL_ShowOpenFileDialog(fileopen_callback, &editor, window, nullptr, 0, nullptr, false);
         }
         if(ImGui::MenuItem("Save", "Ctrl-S")){
-          if(openpath == nullptr){
+          if(openIndex == -1){
             SDL_ShowSaveFileDialog(filesaveas_callback, &editor, window, nullptr, 0, nullptr);
           } else {
-            write_to_path(openpath, editor.GetText());
+            write_to_path(openPaths[openIndex], editor.GetText());
+            modifiedPaths[openIndex] = false;
           }
         }
         if(ImGui::MenuItem("Save As...", "Ctrl-Shift-S")){
@@ -135,10 +141,49 @@ int main(void) {
         }
         ImGui::EndMenu();
       }
+      if(ImGui::BeginMenu("Run")){
+          if (ImGui::MenuItem("Run File", "F6")){
+              //Integrate compiler/interpreter
+          }
+          ImGui::EndMenu();
+      }
       ImGui::EndMenuBar();
     }
 
-    // Display some standard text, called from the Rust compiler.
+    if (openIndex == -1){
+        ImGui::Selectable("  ^ Open a file", false, ImGuiSelectableFlags_::ImGuiSelectableFlags_Disabled, ImVec2(150, 20));
+    } else {
+        int openAtRender = openIndex;
+        for (int i = 0; i < openPaths.size(); ++i) {
+            ImGui::SetCursorPosX(0);
+
+            int lastSlash = -1;
+            int pathLength = strlen(openPaths[i]);
+            for (int j = 0; j < pathLength; ++j) {
+                if(openPaths[i][j] == '/'){
+                    lastSlash = j;
+                }
+            }
+            bool modified = modifiedPaths[i];
+            if (modified)
+                ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(.3f, .4f, 1, 1));
+            ImGui::PushID(openPaths[i]);
+            if(ImGui::Selectable(openPaths[i] + lastSlash + 1, openAtRender == i, 0, ImVec2(150, 20))){
+                std::string editorContent = editor.GetText();
+                contents[openIndex] = editorContent.substr(0, editorContent.size() - 1);
+                openIndex = i;
+                editor.SetText(contents.size() == openPaths.size() ? contents[openIndex] : load_from_path(openPaths[i]));
+            }
+            ImGui::PopID();
+            if (modified)
+                ImGui::PopStyleColor();
+        }
+    }
+    ImGui::SetCursorPos(ImVec2(150, 20));
+    editor.Render("TextEditor", ImVec2(450,580));
+    if(ImGui::IsItemFocused() && keyUpInFrame && openIndex != -1){
+        modifiedPaths[openIndex] = true;
+    }
     editor.Render("TextEditor");
     // Signal to ImGui the end of the current window.
     ImGui::End();
@@ -171,19 +216,34 @@ static void SDLCALL fileopen_callback(void* userdata, const char* const* filelis
     sem_post(&renderSemaphore);
     return;
   }
-  if(*filelist == nullptr || *filelist == ""){
+  if(*filelist == nullptr || *filelist[0] == '\0'){
     std::cerr << "Someone canceled..." << std::endl;
     sem_post(&renderSemaphore);
     return;
   }
-  std::ifstream file(filelist[0]);
-  if(file){
-    std::ostringstream file_content;
-    file_content << file.rdbuf();
-    (*((TextEditor *)userdata)).SetText(file_content.str());
-  } else {
-    std::cerr << "Can't open the file that is at \"" << filelist[0] << "\".";
+
+    if (openIndex != -1)
+        contents[openIndex] = ((TextEditor*)userdata)->GetText();
+  for(int i = 0; i < openPaths.size(); ++i){
+      if (!strcmp(openPaths[i], filelist[0])){
+          std::cout << "Open at pos " << i << std::endl;
+          openIndex = i;
+          ((TextEditor*)userdata)->SetText(load_from_path(openPaths[i]));
+          sem_post(&renderSemaphore);
+          return;
+      }
   }
+  std::string fileContent = load_from_path(filelist[0]);
+
+  char* openedPath = (char*)malloc(sizeof(char) * (strlen(filelist[0]) + 1));
+  strcpy(openedPath, filelist[0]);
+  openPaths.push_back(openedPath);
+  contents.push_back(fileContent);
+
+  openIndex = openPaths.size() - 1;
+  modifiedPaths.push_back(false);
+
+  ((TextEditor*)userdata)->SetText(load_from_path(openedPath));
   sem_post(&renderSemaphore);
   return;
 }
@@ -199,10 +259,18 @@ static void SDLCALL filesaveas_callback(void* userdata, const char* const* filel
     sem_post(&renderSemaphore);
     return;
   }
-  write_to_path(filelist[0], (*((TextEditor *)userdata)).GetText());
-  free(openpath);
-  openpath = (char*)malloc(sizeof(char) * (strlen(filelist[0]) + 1));
-  strcpy(openpath, filelist[0]);
+  std::string content = (*((TextEditor *)userdata)).GetText();
+  write_to_path(filelist[0], content);
+
+  char* openedPath = (char*)malloc(sizeof(char) * (strlen(filelist[0]) + 1));
+  strcpy(openedPath, filelist[0]);
+  openPaths.push_back(openedPath);
+
+  if (openIndex != -1)
+      contents[openIndex] = ((TextEditor*)userdata)->GetText();
+  openIndex = openPaths.size() - 1;
+  modifiedPaths.push_back(false);
+  contents.push_back(content);
   sem_post(&renderSemaphore);
 }
 static void write_to_path(const char* const path, std::string text){
@@ -212,4 +280,15 @@ static void write_to_path(const char* const path, std::string text){
   } else {
     std::cerr << "Unable to write to the file that is at \"" << path << "\".";
   }
+}
+static std::string load_from_path(const char* const path){
+    std::ifstream file(path);
+    if(file){
+        std::ostringstream file_content;
+        file_content << file.rdbuf();
+        return file_content.str();
+    } else {
+        std::cerr << "Can't open the file that is at \"" << path << "\".";
+    }
+    return "Error loading file";
 }
